5f794c3bf9ad75e0abed6c2f498c2ecc50af87e6
Akshat Singh <nitfactor@gmail.com>
2026-06-10 12:14:36 +0530

    main

--- backend/models.py
+++ backend/models.py
@@ -1,2 +1,37 @@
 from sqlalchemy import Column, Integer, String, Date
 from database import Base
+
+class EligibleSecurities(Base):
+    __tablename__ = "eligible_securities"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Date)
+    symbol = Column(String)
+    series = Column(String)
+    normal_eligibility = Column(String)
+    recall_eligibility = Column(String)
+    repay_eligibility = Column(String)
+    market_type = Column(String)
+
+class Foreclosure(Base):
+    __tablename__ = "foreclosure"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Date)
+    symbol = Column(String)
+    series = Column(String)
+    announcement_date = Column(String)
+    record_date = Column(String)
+    shut_period_start = Column(String)
+    shut_period_end = Column(String)
+    next_trade_date = Column(String)
+    description = Column(String)
+
+class OpenPositions(Base):
+    __tablename__ = "open_positions"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Date)
+    symbol = Column(String)
+    series = Column(String)
+    outstanding_quantity = Column(Integer)

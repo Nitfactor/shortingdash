0c77ec82d08d6a082ee42330903d176646ff3d07
Akshat Singh <nitfactor@gmail.com>
2026-06-19 12:20:13 +0530

    main

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:8000",
-        "https://shortingdash.vercel.app"
+        "https://shortingdash.vercel.app",
+        "https://shortingdash.com"
+        "https://www.shortingdash.com"
+
         ],
         allow_credentials = True,
         allow_methods=["*"],
